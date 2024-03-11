b3f7201e7e133e16dcfbbf088cd3537831982c7d
Pryviet <62222839+barty5260@users.noreply.github.com>
2024-03-11 23:20:53 +0100

    previous module exports

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-app/'
-    : '/'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/vue-app/'
+//     : '/'
+// }
+
+module.exports = defineConfig({transpileDependencies: true,publicPath: '/vue-app/'})
