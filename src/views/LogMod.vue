cbf7ae1786f07c41cdf29c6508c3a2ccba55414e
Lang <sergiobenllochlluch@gmail.com>
2025-03-17 18:59:49 +0100

    fixed flex panels

--- src/views/LogMod.vue
+++ src/views/LogMod.vue
@@ -1,50 +1,68 @@
 <template>
-    <div class="log-details">
-      <h2>Log Details</h2>
-      <div>
-        <label for="logSelect">Select Log:</label>
-        <select id="logSelect" v-model="selectedLogId">
-          <option value="">Select a log...</option>
-          <option v-for="log in logs" :key="log.id" :value="log.id">{{ log.content }}</option>
-        </select>
-      </div>
-      <div v-if="log">
-        <div>
-          <label for="content">Content:</label>
-          <input type="text" id="content" v-model="log.content" />
+  <div class="app">
+    <h1 class="app-title">KanbanVue</h1>
+    <div class="separator"></div>
+    <div class="panel-container">
+      <div class="center-panel">
+
+        <div class="log-details">
+          <h2>Log Details</h2>
+          <div>
+            <label for="logSelect">Select Log:</label>
+            <select id="logSelect" v-model="selectedLogId">
+              <option value="">Select a log...</option>
+              <option v-for="log in logs" :key="log.id" :value="log.id">{{ log.content }}</option>
+            </select>
+          </div>
+          <div v-if="log">
+            <div>
+              <label for="content">Content:</label>
+              <input type="text" id="content" v-model="log.content" />
+            </div>
+            <div>
+              <label for="extraInfo">Extra Information:</label>
+              <textarea id="extraInfo" v-model="log.extraInfo"></textarea>
+            </div>
+            <div>
+              <label for="timestamp">Timestamp:</label>
+              <input type="text" id="timestamp" v-model="log.timestamp" @input="validateTimestamp" />
+              <span v-if="!isTimestampValid" class="error-message">Please enter a valid timestamp format (DD/MM/YYYY, HH:MM:SS).</span>
+            </div>
+            <div>
+              <label for="isTask">Is this log a task?:</label>
+              <select id="isTask" v-model="log.isTask">
+                <option :value="true">Yes</option>
+                <option :value="false">No</option>
+              </select>
+            </div>
+            <div>
+            <label for="type">What type of log is this?:</label>
+            <select id="type" v-model="log.type">
+              <option value="ToDo">ToDo</option>
+              <option value="Epic">Epic</option>
+              <option value="Chaser">Chaser</option>
+            </select>
+          </div>
+            <button @click="saveLogChanges">Save Changes</button>
+          </div>
+          <div v-else>
+            <p>No log selected.</p>
+          </div>
+          <button @click="$router.push('/')" class="goto-item-button">Dashboard</button>
+          <button @click="$router.push('/kanban')" class="goto-item-button">Kanban Board</button>
         </div>
-        <div>
-          <label for="extraInfo">Extra Information:</label>
-          <textarea id="extraInfo" v-model="log.extraInfo"></textarea>
-        </div>
-        <div>
-          <label for="timestamp">Timestamp:</label>
-          <input type="text" id="timestamp" v-model="log.timestamp" @input="validateTimestamp" />
-          <span v-if="!isTimestampValid" class="error-message">Please enter a valid timestamp format (DD/MM/YYYY, HH:MM:SS).</span>
-        </div>
-        <div>
-          <label for="isTask">Is this log a task?:</label>
-          <select id="isTask" v-model="log.isTask">
-            <option :value="true">Yes</option>
-            <option :value="false">No</option>
-          </select>
-        </div>
-        <div>
-        <label for="type">What type of log is this?:</label>
-        <select id="type" v-model="log.type">
-          <option value="ToDo">ToDo</option>
-          <option value="Epic">Epic</option>
-          <option value="Chaser">Chaser</option>
-        </select>
       </div>
-        <button @click="saveLogChanges">Save Changes</button>
+
+      <!-- Right Panel (Router Panel) -->
+      <div class="right-panel">
+        <div class="router-panel">
+          <h2>Router Panel</h2>
+          <button @click="$router.push('/')" class="goto-item-button">Dashboard</button>
+          <button @click="$router.push('/kanban')" class="goto-item-button">Kanban Board</button>
+        </div>
       </div>
-      <div v-else>
-        <p>No log selected.</p>
-      </div>
-      <button @click="$router.push('/')" class="goto-item-button">Dashboard</button>
-      <button @click="$router.push('/kanban')" class="goto-item-button">Kanban Board</button>
     </div>
+  </div>
 
   <!-- MainDisclaimer component -->
   <MainDisclaimer />
